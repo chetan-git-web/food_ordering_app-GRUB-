8e2a57595925ae11b743250b334048401a467a28
Kandala Chetan Kumar <chetankandala2017@gmail.com>
2024-04-05 15:37:33 +0530

    adding elastic mail to webpage

--- Frontend/src/components/Contact.js
+++ Frontend/src/components/Contact.js
@@ -1,5 +1,6 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 import { Switch } from '@headlessui/react'
+import { reload } from "firebase/auth"
 
 
 function classNames(...classes) {
@@ -8,18 +9,51 @@ function classNames(...classes) {
 
 const Contact=()=> {
   const [agreed, setAgreed] = useState(false)
+  const fname = useRef("");
+  const lname = useRef("");
+  const email = useRef("");
+  const phoneNo = useRef("");
+  const message = useRef("");
+  console.log(fname);
+  const body = `fname : ${fname.current.value} 
+  lname : ${lname.current.value} 
+  email : ${email.current.value} 
+  phoneNo : ${phoneNo.current.value} 
+  message : ${message.current.value}`
+
+  const emailsend =()=>{
+    Email.send({
+      Host : "smtp.elasticemail.com",
+      Username : "chetankandala2017@gmail.com",
+      Password : "5BEBCFB87B0B9A7769FF62D899FFCFDCBADF",
+      To : 'chetankandala2017@gmail.com',
+      From : "chetankandala2017@gmail.com",
+      Subject : "Grub Contact us mail",
+      Body : body
+  }).then(
+    message => {
+      if(message === "OK"){
+        alert("email sent to admin... will contact you soon");
+      }
+      else{
+        alert("message");
+      }
+    }
+  ).catch(
+    e => alert(e)
+  )
+
+  }
   // contact page
 
   return (
     <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
       
       <div className="mx-auto max-w-2xl text-center">
-        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact sales</h2>
-        <p className="mt-2 text-lg leading-8 text-gray-600">
-          Aute magna irure deserunt veniam aliqua magna enim voluptate.
-        </p>
+        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact Us</h2>
+        
       </div>
-      <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
+      <form className="mx-auto mt-16 max-w-xl sm:mt-20">
         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
           <div>
             <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">
@@ -27,6 +61,7 @@ const Contact=()=> {
             </label>
             <div className="mt-2.5">
               <input
+              ref={fname}
                 type="text"
                 name="first-name"
                 id="first-name"
@@ -41,6 +76,7 @@ const Contact=()=> {
             </label>
             <div className="mt-2.5">
               <input
+              ref={lname}
                 type="text"
                 name="last-name"
                 id="last-name"
@@ -49,26 +85,14 @@ const Contact=()=> {
               />
             </div>
           </div>
-          <div className="sm:col-span-2">
-            <label htmlFor="company" className="block text-sm font-semibold leading-6 text-gray-900">
-              Company
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="text"
-                name="company"
-                id="company"
-                autoComplete="organization"
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
+          
           <div className="sm:col-span-2">
             <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
               Email
             </label>
             <div className="mt-2.5">
               <input
+              ref={email}
                 type="email"
                 name="email"
                 id="email"
@@ -92,14 +116,13 @@ const Contact=()=> {
                   className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
                 >
                   <option>IN</option>
-                  <option>CA</option>
-                  <option>EU</option>
-                  <option>US</option>
+                  
                 </select>
                 
               </div>
               <input
                 type="tel"
+                ref={phoneNo}
                 name="phone-number"
                 id="phone-number"
                 autoComplete="tel"
@@ -113,6 +136,7 @@ const Contact=()=> {
             </label>
             <div className="mt-2.5">
               <textarea
+              ref={message}
                 name="message"
                 id="message"
                 rows={4}
@@ -121,41 +145,16 @@ const Contact=()=> {
               />
             </div>
           </div>
-          <Switch.Group as="div" className="flex gap-x-4 sm:col-span-2">
-            <div className="flex h-6 items-center">
-              <Switch
-                checked={agreed}
-                onChange={setAgreed}
-                className={classNames(
-                  agreed ? 'bg-indigo-600' : 'bg-gray-200',
-                  'flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-                )}
-              >
-                <span className="sr-only">Agree to policies</span>
-                <span
-                  aria-hidden="true"
-                  className={classNames(
-                    agreed ? 'translate-x-3.5' : 'translate-x-0',
-                    'h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out'
-                  )}
-                />
-              </Switch>
-            </div>
-            <Switch.Label className="text-sm leading-6 text-gray-600">
-              By selecting this, you agree to our{' '}
-              <a href="#" className="font-semibold text-indigo-600">
-                privacy&nbsp;policy
-              </a>
-              .
-            </Switch.Label>
-          </Switch.Group>
+          
         </div>
         <div className="mt-10">
           <button
             type="submit"
             onClick={(e)=>{
+              emailsend();
               e.preventDefault();
             }}
+            
             className="block w-full rounded-md bg-red-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             Let's talk
